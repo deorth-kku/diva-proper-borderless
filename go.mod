e3ec70fd6034a54b8173a2028de244dd55c405a6
deorth <deorth@outlook.com>
2023-02-24 13:49:25 +0800

    allow config.toml to configure window

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/deorth-kku/diva-proper-borderless
 
 go 1.20
+
+require github.com/BurntSushi/toml v1.2.1 // indirect
--- mod.go
+++ mod.go
@@ -3,22 +3,45 @@ package main
 import "C"
 
 import (
+	"github.com/BurntSushi/toml"
 	"github.com/deorth-kku/diva-proper-borderless/win32tools"
 	"os"
+	"path"
 	"syscall"
 	"time"
 )
 
-func init() {
+type Config struct {
+	PositionX int32
+	PositionY int32
+	Width     int32
+	Height    int32
+}
+
+//export Init
+func Init() {
+	dir, _ := os.Getwd()
+	conf_path := path.Join(dir, "config.toml")
+	go run(conf_path)
+}
+func run(conf_path string) {
 	pid := os.Getpid()
+	var conf Config
+	toml.DecodeFile(conf_path, &conf)
+
 	var hwnd syscall.Handle
 	var i uint8
-	for int(hwnd) == 0 && i < 100 {
-		time.Sleep(1000 * 1000 * 100)
+	for i < 100 {
 		hwnd, _ = win32tools.FindWindowByPid(pid)
-		i++
+		if int(hwnd) == 0 {
+			time.Sleep(1000 * 1000 * 100)
+			i++
+		} else {
+			break
+		}
+
 	}
-	win32tools.SetBorderless(hwnd, 0, 0, 0, 0)
+	win32tools.SetBorderless(hwnd, conf.PositionX, conf.PositionY, conf.Width, conf.Height)
 }
 
 func main() {
