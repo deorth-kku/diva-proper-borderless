0c28dd4acfdcae6e4b60da86e72ba5cc5293dfe4
deorth <deorth@outlook.com>
2023-03-06 17:52:38 +0800

    avoid searching for hwnd every time

--- mod.go
+++ mod.go
@@ -23,13 +23,12 @@ type Config struct {
 
 var conf_path string
 var pid int
+var hwnd syscall.Handle
 
-//export D3DInit
-func D3DInit() {
+//export Init
+func Init() {
 	dir, _ := os.Getwd()
 	conf_path = path.Join(dir, "config.toml")
-	pid = os.Getpid()
-	fmt.Printf("[Proper Borderless] pid: %d\n", pid)
 	fmt.Printf("[Proper Borderless] using config file: %s\n", conf_path)
 	go run(false)
 }
@@ -48,46 +47,50 @@ func run(is_resize bool) {
 		return
 	}
 
-	var i uint8
-	var hwnd_list []syscall.Handle
-	for i < 100 {
-		var err error
-		hwnd_list, err = win32tools.FindWindowByPid(pid)
-		if err != nil {
-			time.Sleep(1000 * 1000 * 100)
-			i++
-		} else {
-			break
+	// only search for hwnd when global hwnd is not set
+	if int(hwnd) == 0 {
+		pid := os.Getpid()
+		var i int8
+		fmt.Printf("[Proper Borderless] searching windows for pid %d\n", pid)
+		for i < 100 {
+			hwnd = findDivaHwnd(pid)
+			if int(hwnd) == 0 {
+				time.Sleep(1000 * 1000 * 100) //sleep 0.1s
+				i++
+			} else {
+				break
+			}
 		}
 	}
-	fmt.Printf("[Proper Borderless] found %d hwnds:", len(hwnd_list))
-	for _, h := range hwnd_list {
-		fmt.Printf("%d,", h)
-	}
-	fmt.Print("\n")
-
-	var hwnd syscall.Handle
-	for _, h := range hwnd_list {
-		title, err := win32tools.GetWindowText(h, 200)
-		if err != nil {
-			fmt.Printf("[Proper Borderless] get title for %d failed, cause: %s\n", int(h), err)
-		}
-		if strings.HasSuffix(title, "+") {
-			fmt.Printf("[Proper Borderless] using hwnd %d, title: %s\n", int(h), title)
-			hwnd = h
-			break
-		} else {
-			fmt.Printf("[Proper Borderless] skipping hwnd %d, title: %s\n", int(h), title)
-			continue
-		}
-	}
-
 	if !win32tools.IsWindowBorderless(hwnd) {
 		fmt.Printf("[Proper Borderless] setting hwnd %d with (%d,%d,%d,%d)\n", int(hwnd), conf.PositionX, conf.PositionY, conf.Width, conf.Height)
 		win32tools.SetBorderless(hwnd, conf.PositionX, conf.PositionY, conf.Width, conf.Height)
 	} else {
 		fmt.Printf("[Proper Borderless] skipping hwnd %d because it's already borderless\n", int(hwnd))
 	}
+}
+
+func findDivaHwnd(pid int) (result syscall.Handle) {
+	hwnd_list, err := win32tools.FindWindowByPid(pid)
+	if err != nil {
+		return
+	}
+	for _, h := range hwnd_list {
+		title, err := win32tools.GetWindowText(h, 200)
+		if err != nil {
+			fmt.Printf("[Proper Borderless] get title for %d failed, cause: %s\n", int(h), err)
+			return
+		}
+		// search for window that title ends with "+". I know this is stupid but I cannot think of anything else
+		if strings.HasSuffix(title, "+") {
+			fmt.Printf("[Proper Borderless] using hwnd %d, title: %s\n", int(h), title)
+			result = h
+			return
+		} else {
+			fmt.Printf("[Proper Borderless] skipping hwnd %d, title: %s\n", int(h), title)
+		}
+	}
+	return
 }
 
 func main() {
